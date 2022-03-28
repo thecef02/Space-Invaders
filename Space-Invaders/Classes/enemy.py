e00d9a52354845bd767d630437a01e4c58e47c34
Mallory Lee <sein980406@gmail.com>
2022-03-28 17:30:00 -0600

    maintainability

--- Space-Invaders/Classes/enemy.py
+++ Space-Invaders/Classes/enemy.py
@@ -13,6 +13,13 @@ currently set up as the large asteroid
 class Enemy(MovingActor):
     def __init__(self, type = 1):
         super().__init__()
+        self.base_setup()
+        if type == 1:
+            self.type1_setup()
+        if type == 2:
+            self.type2_setup()
+
+    def base_setup(self):
         self.center.x = random.randint(1, SCREEN_WIDTH)
         self.center.y = SCREEN_HEIGHT
         self.velocity.dx = random.uniform(-1 * BIG_ENEMY_SPEED, BIG_ENEMY_SPEED)
@@ -25,12 +32,16 @@ class Enemy(MovingActor):
         self.image = ENEMY_IMAGE_1
         self.texture = arcade.load_texture(self.image)
         self.sound = arcade.Sound(EXPLOSION_SOUND)
-        if type == 2:
-            self.velocity.dx = random.uniform(-6 * BIG_ENEMY_SPEED, 6* BIG_ENEMY_SPEED)
-            self.velocity.dy = random.uniform(-1.5 * BIG_ENEMY_SPEED, -3 * BIG_ENEMY_SPEED)
-            self.image = ENEMY_IMAGE_2
-            self.texture = arcade.load_texture(self.image)
-        
+
+    def type1_setup(self):
+        pass
+
+    def type2_setup(self):
+        self.velocity.dx = random.uniform(-6 * BIG_ENEMY_SPEED, 6* BIG_ENEMY_SPEED)
+        self.velocity.dy = random.uniform(-1.5 * BIG_ENEMY_SPEED, -3 * BIG_ENEMY_SPEED)
+        self.image = ENEMY_IMAGE_2
+        self.texture = arcade.load_texture(self.image)
+
     def draw(self):
         arcade.draw_texture_rectangle(self.center.x, self.center.y, self.texture.width/2, self.texture.height/2, self.texture, self.angle, 255)
 
